8d7ae646b2e9eac9a15cd39f05d345122c78884d
Marcelo Vazquez <mvazquez@makingsense.com>
2017-03-06 12:20:34 -0300

    [MLP-117] Integrate Autofac as IoC container

--- Xamarin/MvvmSeed/MvvmSeed/MvvmSeed.Android/Setup.cs
+++ Xamarin/MvvmSeed/MvvmSeed/MvvmSeed.Android/Setup.cs
@@ -1,7 +1,11 @@
 using Android.Content;
+using Autofac;
+using Autofac.Extras.MvvmCross;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Droid.Platform;
+using MvvmCross.Platform.IoC;
 using MvvmSeed.Application;
+using MvvmSeed.Application.Modules;
 
 namespace MvvmSeed.Android
 {
@@ -9,9 +13,21 @@ namespace MvvmSeed.Android
     {
         public Setup(Context applicationContext) : base(applicationContext) { }
 
-        protected override IMvxApplication CreateApp()
+        protected override IMvxApplication CreateApp() => new App();
+
+        protected override IMvxIoCProvider CreateIocProvider()
         {
-            return new App();
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<ApplicationModule>();
+
+            // This is an important step that ensures all the ViewModel's are loaded into the container. Without it, MvvmCross wouldn't register them by itself; needs more investigation.
+            var viewModelsAssembly = typeof(App).Assembly;
+            containerBuilder.RegisterAssemblyTypes(viewModelsAssembly)
+                .AssignableTo<MvxViewModel>()
+                .As<IMvxViewModel, MvxViewModel>()
+                .AsSelf();
+
+            return new AutofacMvxIocProvider(containerBuilder.Build());
         }
     }
 }
---
+++ Xamarin/MvvmSeed/MvvmSeed/MvvmSeed.Application/Modules/ApplicationModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using MvvmCross.Core.ViewModels;
+using MvvmSeed.Application.ViewModels;
+
+namespace MvvmSeed.Application.Modules
+{
+    public class ApplicationModule : Module
+    {
+        protected override void Load(ContainerBuilder cb)
+        {
+            cb.Register(c => new MvxAppStart<SampleViewModel>()).As<IMvxAppStart>().SingleInstance();
+        }
+    }
+}
