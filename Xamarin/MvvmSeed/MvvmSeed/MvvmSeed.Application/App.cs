45d1e47f4e8fcb7365f6d875ce12c58599488dc3
Marcelo Vazquez <mvazquez@makingsense.com>
2017-03-06 12:20:34 -0300

    [MLP-117] Integrate Autofac as IoC container

--- Xamarin/MvvmSeed/MvvmSeed/MvvmSeed.Application/App.cs
+++ Xamarin/MvvmSeed/MvvmSeed/MvvmSeed.Application/App.cs
@@ -1,15 +1,9 @@
 ﻿using MvvmCross.Core.ViewModels;
-using MvvmCross.Platform;
-using MvvmSeed.Application.ViewModels;
 
 namespace MvvmSeed.Application
 {
     public class App : MvxApplication
     {
-        public App()
-        {
-            //TODO - Integrate Autofac and create separate modules
-            Mvx.RegisterSingleton<IMvxAppStart>(new MvxAppStart<SampleViewModel>());
-        }
+        //Placeholder for app-specific configurations
     }
 }
