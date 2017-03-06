8b5c9cd98e2d416523f7962fc4392ca61cda4005
Marcelo Vazquez <mvazquez@makingsense.com>
2017-03-06 13:55:56 -0300

    [MLP-119] Integrate RealmDB

--- Xamarin/MvvmSeed/MvvmSeed/MvvmSeed.Application/ViewModels/SampleViewModel.cs
+++ Xamarin/MvvmSeed/MvvmSeed/MvvmSeed.Application/ViewModels/SampleViewModel.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Linq;
-using MvvmCross.Core.ViewModels;
+﻿using MvvmCross.Core.ViewModels;
 using MvvmSeed.Application.ViewModels.Interfaces;
+using MvvmSeed.Domain.Services;
 
 namespace MvvmSeed.Application.ViewModels
 {
     public class SampleViewModel : MvxViewModel, ISampleViewModel
     {
-        public SampleViewModel()
+        private readonly IStringRandomizerService _stringRandomizerService;
+
+        public SampleViewModel(IStringRandomizerService stringRandomizerService)
         {
+            _stringRandomizerService = stringRandomizerService;
+
             DoSomethingCommand = new MvxCommand(DoSomethingCommandExecute);
+            SampleString = _stringRandomizerService.LastRandomizedValue;
         }
 
         public IMvxCommand DoSomethingCommand { get; }
@@ -19,13 +23,11 @@ namespace MvvmSeed.Application.ViewModels
             get { return _sampleString; }
             set { _sampleString = value; RaisePropertyChanged(); }
         }
-        private string _sampleString = "Hello World!";
+        private string _sampleString;
 
         private void DoSomethingCommandExecute()
         {
-            //Sample command, just randomly re-arranges SampleString to check databinding works properly
-            var random = new Random();
-            SampleString = new string(SampleString.ToCharArray().OrderBy(s => random.Next(2) % 2 == 0).ToArray());
+            SampleString = _stringRandomizerService.RandomizeString(SampleString);
         }
     }
 }
