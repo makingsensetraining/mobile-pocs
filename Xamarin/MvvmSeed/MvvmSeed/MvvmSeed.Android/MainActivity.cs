6c09174d1626c6c7bb57da6da8b54b5e12af117d
Marcelo Vazquez <mvazquez@makingsense.com>
2017-03-06 09:48:01 -0300

    [MLP-115] Create Xamarin seed project

---
+++ Xamarin/MvvmSeed/MvvmSeed/MvvmSeed.Android/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace MvvmSeed.Android
+{
+    [Activity(Label = "MvvmSeed.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
