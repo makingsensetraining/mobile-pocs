06724be07c722b51e1fd043021a4fc237f2909c4
Marcelo Vazquez <mvazquez@makingsense.com>
2017-03-20 10:20:52 -0300

    [MLP-149] Added base Swift Seed Project and MSButton Custom UI Component

---
+++ IOS/SwiftSeedProject/SwiftSeedProject/Interfaces/ViewControllers/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  SwiftSeedProject
+//
+//  Created by Mugu on 3/17/17.
+//  Copyright © 2017 Making Sense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController { }
+
