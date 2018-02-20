bc260a98bfb5672015fd1f339135ec1ef374a79b
Unknown <lpelizza@makingsense.com>
2018-02-20 14:26:56 -0300

    [ CAP-212] Add login with Redux

--- ReactNative/groceryList/src/App.js
+++ ReactNative/groceryList/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginScreen from './screens/LoginScreen';
 
 class App extends Component {
 
@@ -21,12 +22,8 @@ class App extends Component {
 
 	render() {
 		return (
-			<Provider store={createStore(reducers)}>
-				<View>
-					<Text>
-						Hello!
-					</Text>
-				</View>
+			<Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+				<LoginScreen />
 			</Provider>
 		);
 	}
