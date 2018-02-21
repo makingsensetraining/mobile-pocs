62512701e84fa0c22f3bce1d8b74640a01470389
lpelizzaMS <lpelizza@makingsense.com>
2018-02-21 10:46:49 -0300

    [CAP-212] Add firebase (#28)

--- ReactNative/groceryList/src/App.js
+++ ReactNative/groceryList/src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+
+	componentWillMount() {
+		const config = {
+			apiKey: 'AIzaSyDhmJzDYrO42SDd1LqFRpILfd6k2wvDSm8',
+			authDomain: 'grocerylist-3fc84.firebaseapp.com',
+			databaseURL: 'https://grocerylist-3fc84.firebaseio.com',
+			projectId: 'grocerylist-3fc84',
+			storageBucket: 'grocerylist-3fc84.appspot.com',
+			messagingSenderId: '1006599893814'
+		};
+		firebase.initializeApp(config);
+	}
+  
 	render() {
 		return (
 			<Provider store={createStore(reducers)}>
