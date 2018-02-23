a5b3d5b9bc2f344c6ead55d98917e79fd2207550
lpelizzaMS <lpelizza@makingsense.com>
2018-02-23 08:52:22 -0300

    [CAP-212] Add logic to edit a product

--- ReactNative/groceryList/src/Router.js
+++ ReactNative/groceryList/src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginScreen from './screens/LoginScreen';
 import ProductsScreen from './screens/ProductsScreen';
 import CreateProductScreen from './screens/CreateProductScreen';
+import EditProductScreen from './screens/EditProductScreen';
 
 const RouterComponent = () => {
   return (
@@ -12,20 +13,21 @@ const RouterComponent = () => {
           <Scene key="loginScreen" component={LoginScreen} title="Please Login" />
         </Scene>
 
-        <Scene key="mainFlow">
-          <Scene 
-          rightTitle = "Add"
-          onRight={() => { Actions.createProductScreen() }}
-          key="productsScreen" 
-          component={ProductsScreen}
-          title="Products"
-          initial
-          />
-          <Scene key="createProductScreen" component={CreateProductScreen} title="Create Product" />
-        </Scene>
-      </Scene>
-    </Router>
-  );
+				<Scene key="mainFlow">
+					<Scene 
+					rightTitle = "Add"
+					onRight={() => { Actions.createProductScreen() }}
+					key="productsScreen" 
+					component={ProductsScreen}
+					title="Products"
+					initial
+					/>
+					<Scene key="createProductScreen" component={CreateProductScreen} title="Create Product" />
+					<Scene key="editProductScreen" component={EditProductScreen} title="Edit Product" />
+				</Scene>
+			</Scene>
+		</Router>
+	);
 };
 
 export default RouterComponent;
