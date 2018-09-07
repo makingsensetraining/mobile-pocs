c8a476d44eb89d3e5d2ca2d897750bb2897de68e
Brian Sztamfater <briansztamfater@MacBook-Pro-de-Brian.local>
2018-09-07 13:46:42 -0300

    [React Native] Background Fetch PoC

---
+++ ReactNative/BackgroundTask/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp4fe7ab4de23b47c7ace278f3d0c9575d://192.168.0.50:19002";
+
+}
